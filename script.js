59c6b0210611d85682226719e0d178ddb84ecabf
Taron <79373250+taron0@users.noreply.github.com>
2021-03-20 20:38:33 +0400

    Create Countries api

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ searchCountry.addEventListener("input", function (event) {
   fetch(`https://restcountries.eu/rest/v2/name/${searchValue}`)
     .then((ressponse) => ressponse.json())
     .then((r) => {
+        
       container.innerHTML = "";
       r.forEach(({ name, flag }) => {
         const div = document.createElement("div");
         const li = document.createElement("li");
+        
         li.className = "list";
         li.textContent = name;
         console.log(name);
@@ -17,14 +19,17 @@ searchCountry.addEventListener("input", function (event) {
         img.setAttribute("src", flag);
         div.append(li);
         div.append(img);
+  
 
         container.appendChild(div);
+       console.log(r) 
       });
     });
 });
 fetch("https://restcountries.eu/rest/v2/all")
   .then((response) => response.json())
   .then((countries) => {
+      console.log(countries[0].region);
     countries.forEach(({ name, flag }) => {
       const div = document.createElement("div");
       const li = document.createElement("li");
@@ -40,3 +45,4 @@ fetch("https://restcountries.eu/rest/v2/all")
     });
   })
   .catch((err) => console.log(err));
+
